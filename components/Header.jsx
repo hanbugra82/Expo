a77ed2ed7c5ddfd0cd383afcca83598e4e12a42b
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2024-09-20 13:56:33 +0300

    style: completed goal icons added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,31 @@
-import { Text, View, StyleSheet } from "react-native"
-export default function Header(){
-    return(
-        <>
-            <View style={styles.header}>
-                <Text style={styles.greetingText}>Welcome Back, Berktug!</Text>
-            </View>
-        </>
-    )
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+export default function Header() {
+  return (
+    <>
+      <View style={styles.header}>
+        <Text style={styles.greetingText}>Welcome Back!</Text>
+        <Pressable>
+          <Feather name="align-justify" style={styles.icon} />
+        </Pressable>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    header:{
-        display:"flex",
-        margin:16
-    },
-    greetingText:{
-        fontWeight:"bold",
-        fontSize:"24"
-    }
-})
+  header: {
+    display: "flex",
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between",
+    margin: 16,
+  },
+  icon: {
+    fontSize: 24,
+  },
+  greetingText: {
+    fontWeight: "bold",
+    fontSize: "24",
+  },
+});
